2bafbbf023d0682b26c772a633319ac80fbb6117
Giancarlo Vega Marin <jungianca6@estudiantec.cr>
2024-10-02 21:35:31 -0600

    union en topmodule

--- Proyecto Grupal 1/Tarea1/calculadoraFPGA.sv
+++ Proyecto Grupal 1/Tarea1/calculadoraFPGA.sv
@@ -1,9 +1,10 @@
 module calculadoraFPGA (
 		input logic a,b,c,d,
+		input clk,
+		input reset,
 		output logic [2:0] resultado,
 		output logic [3:0] display,
-		output logic y1,
-      output  logic y0
+		output logic y1, y0, yb1,yb0
 );
 
 	decoderFingers decoder1 (
@@ -14,7 +15,16 @@ module calculadoraFPGA (
         .y1(y1),
         .y0(y0)
     );
-
-
-	
+	 
+	 decoderAcumulator acumulator (
+        .clk(clk),
+        .reset(reset),
+        .y1(y1),
+        .y0(y0),
+        .y3(y3),
+        .y2(y2),
+		  .yb1(yb1),
+		  .yb0(yb0)
+    );
+	 
 endmodule
