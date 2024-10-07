d150606fe8f2b2c83cb57cb0482b9d52455480bf
Giancarlo Vega Marin <jungianca6@estudiantec.cr>
2024-10-06 20:18:44 -0600

    nombre de carpeta
    quartus se pone con varas si el nombre tiene espacios

---
+++ ProyectoG1/Tarea1/decoderFingers.sv
@@ -0,0 +1,20 @@
+module decoderFingers (
+    input logic a, b, c, d,
+    output logic y1, y0
+);
+
+    wire NOTa, NOTc, NOTac, NOTab;
+
+    // Descripción de las compuertas lógicas
+    not (NOTa, a);            
+    not (NOTc, c);            
+	 
+    and (NOTac, NOTa, c);         
+    and (NOTab, NOTa, b);        
+	 
+    or  (y0, NOTc, NOTab);     
+    assign y1 = NOTac; 
+
+
+endmodule
+
